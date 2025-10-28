56a4194dbfdba45bfc982a48fb5330013cfa395a
shan <dharshanpk.2003@gmail.com>
2025-10-28 21:46:09 +0530

    Fix wildcard route for Express5 and update build script

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
   const frontendPath = path.join(__dirname, '../Frontend/dist');
   app.use(express.static(frontendPath));
 
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
 });
 }
